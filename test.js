ad7d5e769cacb457144a6c8748983970fb29fe4a
Jimena_Gensollen <jimenagensollen@gmail.com>
2020-05-05 15:42:06 -0400

    added babel cli for testing

--- test.js
+++ test.js
@@ -3,13 +3,13 @@ const accordion = require('./server/index.js');
 const request = supertest(accordion)
 
 
-accordion.get('api/accordion/', async (req, res) => {
-    res.json({message: 'pass!'})
+accordion.get('api/accordion/', async (req, response) => {
+    response.json({message: 'pass!'})
 });
 
 describe('GET/Request', () => {
     test('Should get a response from a get request', async done => {
-        const res = await request.get('/api/accordion/')
+        const response = await request.get('/api/accordion/')
         
         expect(response.body.message).toBe('pass!');
         done();
