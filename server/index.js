f102f7ec3865ce5a7504fe9a8e3f89614f2d15b9
Jimena_Gensollen <jimenagensollen@gmail.com>
2020-05-05 16:36:00 -0400

    fixed w/linter

--- server/index.js
+++ server/index.js
@@ -1,29 +1,29 @@
 const express = require('express');
+
 const accordion = express();
 const path = require('path');
 
 const db = require('../data/db/accordion');
 
-//const port = 3004;
+// const port = 3004;
 accordion.use(express.static(path.join(__dirname, '../public')));
 accordion.use(express.json());
 
-//for testing purposes
+// for testing purposes
 accordion.get('/', async (req, res) => {
-   res.send('pass!')
+  res.send('pass!');
 });
 //
 
-accordion.get('/api/accordion/:id',function (req,res) {
-   var id = req.params;
-    
-    db.findOne({ 'features.id' : `${id.id}` }, function (err, feature) {
-    if(err) console.log('errordatA');
-    console.log('%s is the id', feature)
-    res.send(feature)
-   })
-})
+accordion.get('/api/accordion/:id', (req, res) => {
+  const id = req.params;
+  db.findOne({ 'features.id': `${id.id}` }, (err, feature) => {
+    if (err) console.log('errordatA');
+    console.log('%s is the id', feature);
+    res.send(feature);
+  });
+});
 
-//accordion.listen(port,() =>console.log(`Listening on port ${port}`));
+// accordion.listen(port,() =>console.log(`Listening on port ${port}`));
 
 module.exports = accordion;
